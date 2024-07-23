e6b1c1006b92af0e146e17ecf04e7a4ec3306228
Rob2043 <135964485+Rob2043@users.noreply.github.com>
2024-07-23 19:57:20 +0200

    little update

--- Assets/Scripts/Main Activetes/Main Choise.cs
+++ Assets/Scripts/Main Activetes/Main Choise.cs
@@ -9,16 +9,19 @@ public class MainChoise : MonoBehaviour
 {
     [SerializeField] private float _time;
     [SerializeField] private TMP_Text[] _arrayTextNumbers = new TMP_Text[8];
+    [SerializeField] private TMP_Text _textOfGuessNumber;
+    [SerializeField] private TMP_Text _textOfHidNumber;
     private Dictionary<int, int> RemeberNumber = new Dictionary<int, int>();
     private int[] RandomNumbers = new int[8];
     private int _choiseNumber = 0;
-    private int _countOfNumbers = Random.Range(6, 8);
+    private int _countOfNumbers;
     private int[] _guessNumnber;
     private int _guessDigit = 0;
     private int _hiddenNumber = 3;
 
     private void Awake()
     {
+        _countOfNumbers = Random.Range(6, 8);
         _guessNumnber = new int[_countOfNumbers];
         _hiddenNumber = _countOfNumbers switch
         {
@@ -42,20 +45,40 @@ public class MainChoise : MonoBehaviour
                 _copyHiddenNumber--;
             }
         }
+        _textOfGuessNumber.text = $"{_guessDigit}";
+        int RandomPeriodNumber = Random.Range(0, 7);
         for (int i = 0; i < RandomNumbers.Length; i++)
         {
             int _randomNumber = RandomNumbers[i];
-            int _guessRandomDigit = 0;
-            for (int j = 0; j < _countOfNumbers; j++)
+            if (i == RandomPeriodNumber)
+                _randomNumber = _guessDigit;
+            else
             {
-                _guessRandomDigit = Random.Range(0, 9);
-                if (_randomNumber == 0)
-                    _randomNumber = _guessRandomDigit;
-                else
-                    _randomNumber = _randomNumber * 10 + _guessRandomDigit;
+                int _guessRandomDigit = 0;
+                for (int j = 0; j < _countOfNumbers; j++)
+                {
+                    _guessRandomDigit = Random.Range(0, 9);
+                    if (_randomNumber == 0)
+                        _randomNumber = _guessRandomDigit;
+                    else
+                        _randomNumber = _randomNumber * 10 + _guessRandomDigit;
+                }
             }
             _arrayTextNumbers[i].text = $"{_randomNumber}";
-            RandomNumbers[i] = _randomNumber; 
+            RandomNumbers[i] = _randomNumber;
         }
+    }
+
+    private int ToNummber(int[] EnterArray)
+    {
+        int _hidNumber = 0;
+        for (int i = 0; i < EnterArray.Length; i++)
+        {
+            if (_hidNumber == 0)
+                _hidNumber = EnterArray[i];
+            else
+                _hidNumber = _hidNumber * 10 + _guessNumnber[i];
+        }
+        return _hidNumber;
     }
 }
