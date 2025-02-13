7ef88b1c6e1c015447626cefa64ccf163491a453
Rob2043 <135964485+Rob2043@users.noreply.github.com>
2025-02-13 20:18:57 +0100

    add basis monetization

--- Assets/Scripts/Ads/AdsShow.cs
+++ Assets/Scripts/Ads/AdsShow.cs
@@ -1,14 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using CustomEventBus;
+using CrazyGames;
 
 public class AdsShow : MonoBehaviour
 {
 
-    void Start()
+    void Awake()
     {
-
+        if (CrazySDK.IsAvailable)
+        {
+            CrazySDK.Init(() => { });
+        }
     }
     private void OnEnable()
     {
@@ -22,10 +24,40 @@ public class AdsShow : MonoBehaviour
     }
     private void ShowAds()
     {
-
+        CrazySDK.Ad.RequestAd(
+            CrazyAdType.Midgame,
+            () =>
+            {
+                Debug.Log("Midgame ad started");
+            },
+            (error) =>
+            {
+                Debug.Log("Midgame ad error: " + error);
+            },
+            () =>
+            {
+                Debug.Log("Midgame ad finished");
+            }
+        );
     }
     private void RewardAds()
     {
-
+        CrazySDK.Ad.RequestAd(
+            CrazyAdType.Rewarded,
+            () =>
+            {
+                Debug.Log("Rewarded ad started");
+            },
+            (error) =>
+            {
+                Debug.Log("Rewarded ad error: " + error);
+            },
+            () =>
+            {
+                Debug.Log("Rewarded ad finished, reward the player here");
+                EventBus.SetCoins.Invoke(500);
+                EventBus.UpdateMoney.Invoke();
+            }
+        );
     }
 }
